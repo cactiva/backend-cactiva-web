bb1d38d11ae3d2044b8c8de56d5dd25f21656e25
IrulArif <masterirul@gmail.com>
2020-03-23 17:39:12 +0700

    fix port

--- src/payment/invoices.js
+++ src/payment/invoices.js
@@ -1,81 +1,89 @@
-const { Clientdb } = require('../db/db.js')
-const axios = require('axios')
-const {USER_DOESNT_EXISTS} = require('../models/Errors')
-const timeStamp = Math.floor(Date.now()/1000)
+const { Clientdb } = require("../db/db.js");
+const axios = require("axios");
+const { USER_DOESNT_EXISTS } = require("../models/Errors");
+const timeStamp = Math.floor(Date.now() / 1000);
 
 const postInvoice = async (req, res) => {
-    
-    const { email, pay} = req.body
-    const user = await Clientdb.query('SELECT * FROM "UserProfile" WHERE email = $1',[email])
-    const convert = pay * 17000
-    const url = 'http://127.0.0.1:3001/invoice/success/'+email
-    
-    try{
-        
-        if(!user.rows[0]){
-            res.send(new Error(USER_DOESNT_EXISTS))
-        }else{
-        
-        const checkInvId = user.rows[0].invoice_id
-        const pass = user.rows[0].password
-        
-        if(checkInvId != 0){
-            await axios({
-                method: 'GET',
-                url: 'https://api.xendit.co/v2/invoices/'+checkInvId,
-                headers:{ 
-                    'cache-contorl': 'no-cache',
-                    authorization: 'Basic eG5kX2RldmVsb3BtZW50X1U1eERTaWh5M3o0aHdvaEhmM05zMmlOMHVJd01KWWZ3YUtSZGk5VnlEN1RKTk51NTlkYmdpY2I2ZnVXSGFkOg==',
-                }
-            }).then(result =>{
-                if(result.data.status === "PENDING"){
-                    return res.send(result.data.invoice_url)
-                }else if(result.data.status === "SETTLED"){
-                    return res.send("PAID")   
-                }else if(result.data.status === "PAID"){
-                    return res.send("PAID")
-                }else if(result.data.status === "EXPIRED"){
-                    return res.send(createInvoice(timeStamp, email, convert, url))
-                }
-            }).catch(error=>{
-                return res.send(error.result)
-            })
-        }else{
-            return res.send(createInvoice(timeStamp, email, convert, url))
-            }
-        }
-    
-    }catch(err){
-        throw err
-    }
-}
+  const { email, pay } = req.body;
+  const user = await Clientdb.query(
+    'SELECT * FROM "UserProfile" WHERE email = $1',
+    [email]
+  );
+  const convert = pay * 17000;
+  const url = "http://127.0.0.1:10000/invoice/success/" + email;
 
-const createInvoice = async (timeStamp, email, convert, urls) =>{
-    await axios({
-        method: 'POST',
-        url: 'https://api.xendit.co/v2/invoices',
-        headers:{ 
-            'cache-control':'no-status',
-            'content-type': 'application/json',
-            authorization: 'Basic eG5kX2RldmVsb3BtZW50X1U1eERTaWh5M3o0aHdvaEhmM05zMmlOMHVJd01KWWZ3YUtSZGk5VnlEN1RKTk51NTlkYmdpY2I2ZnVXSGFkOg==',
-        },
-        data:{
-            external_id: timeStamp.toString(),
-            amount: convert,
-            payer_email: email,
-            success_redirect_url: urls,
-            description: 'Activation Code Payment',
-            payment_methods: ["CREDIT_CARD"]
-        }
-    }).then(async response => {
-        const idInvoice = response.data.id
-        await Clientdb.query('UPDATE "UserProfile" Set "invoice_id" = $1 where email = $2',[idInvoice, email])
-        
-        return response.data.invoice_url
-    }).catch(err => {
-        return err.response.data
+  try {
+    if (!user.rows[0]) {
+      res.send(new Error(USER_DOESNT_EXISTS));
+    } else {
+      const checkInvId = user.rows[0].invoice_id;
+      const pass = user.rows[0].password;
+
+      if (checkInvId != 0) {
+        await axios({
+          method: "GET",
+          url: "https://api.xendit.co/v2/invoices/" + checkInvId,
+          headers: {
+            "cache-contorl": "no-cache",
+            authorization:
+              "Basic eG5kX2RldmVsb3BtZW50X1U1eERTaWh5M3o0aHdvaEhmM05zMmlOMHVJd01KWWZ3YUtSZGk5VnlEN1RKTk51NTlkYmdpY2I2ZnVXSGFkOg=="
+          }
+        })
+          .then(result => {
+            if (result.data.status === "PENDING") {
+              return res.send(result.data.invoice_url);
+            } else if (result.data.status === "SETTLED") {
+              return res.send("PAID");
+            } else if (result.data.status === "PAID") {
+              return res.send("PAID");
+            } else if (result.data.status === "EXPIRED") {
+              return res.send(createInvoice(timeStamp, email, convert, url));
+            }
+          })
+          .catch(error => {
+            return res.send(error.result);
+          });
+      } else {
+        return res.send(createInvoice(timeStamp, email, convert, url));
+      }
+    }
+  } catch (err) {
+    throw err;
+  }
+};
+
+const createInvoice = async (timeStamp, email, convert, urls) => {
+  await axios({
+    method: "POST",
+    url: "https://api.xendit.co/v2/invoices",
+    headers: {
+      "cache-control": "no-status",
+      "content-type": "application/json",
+      authorization:
+        "Basic eG5kX2RldmVsb3BtZW50X1U1eERTaWh5M3o0aHdvaEhmM05zMmlOMHVJd01KWWZ3YUtSZGk5VnlEN1RKTk51NTlkYmdpY2I2ZnVXSGFkOg=="
+    },
+    data: {
+      external_id: timeStamp.toString(),
+      amount: convert,
+      payer_email: email,
+      success_redirect_url: urls,
+      description: "Activation Code Payment",
+      payment_methods: ["CREDIT_CARD"]
+    }
+  })
+    .then(async response => {
+      const idInvoice = response.data.id;
+      await Clientdb.query(
+        'UPDATE "UserProfile" Set "invoice_id" = $1 where email = $2',
+        [idInvoice, email]
+      );
+
+      return response.data.invoice_url;
     })
-}
+    .catch(err => {
+      return err.response.data;
+    });
+};
 
 // const comparePassword = (candidatePassword, passwordmore) => {
 //     if(bcrypt.compareSync(candidatePassword,passwordmore)){
@@ -86,17 +94,19 @@ const createInvoice = async (timeStamp, email, convert, urls) =>{
 //   }
 
 const validateInvoice = {
-    schema:{
-        body:{
-            type: 'object',
-            properties:{
-                email: { type: 'string', format: 'email'}
-            },
-            required: ['email']
-        }
+  schema: {
+    body: {
+      type: "object",
+      properties: {
+        email: { type: "string", format: "email" }
+      },
+      required: ["email"]
     }
-}
+  }
+};
 
 module.exports = {
-    postInvoice, validateInvoice, createInvoice
-}
+  postInvoice,
+  validateInvoice,
+  createInvoice
+};
--- src/server.js
+++ src/server.js
@@ -1,65 +1,68 @@
-const Fastify = require('fastify')
-const uuid = require('uuid/v4')
-const jwt = require('fastify-jwt')
-const { validatePostLogin, validatePostSignup } = require('./models/auth')
-const { postLogin, postSignup } = require('./controller/postController')
-const { validateInvoice, postInvoice} = require('./payment/invoices')
-const {getSuccess} = require('./payment/succees')
-const { validLicense } = require('./validLicense')
-const {Clientdb} = require('./db/db')
-const {userController} = require("./controller/userController")
-const {getTeamSuccess} = require("./payment/donation/teamsuccess")
-const {getTeamInvoice} = require("./payment/donation/teaminvoice")
+const Fastify = require("fastify");
+const uuid = require("uuid/v4");
+const jwt = require("fastify-jwt");
+const { validatePostLogin, validatePostSignup } = require("./models/auth");
+const { postLogin, postSignup } = require("./controller/postController");
+const { validateInvoice, postInvoice } = require("./payment/invoices");
+const { getSuccess } = require("./payment/succees");
+const { validLicense } = require("./validLicense");
+const { Clientdb } = require("./db/db");
+const { userController } = require("./controller/userController");
+const { getTeamSuccess } = require("./payment/donation/teamsuccess");
+const { getTeamInvoice } = require("./payment/donation/teaminvoice");
 // const {checkrowcount} = require('./controller/checkrowcount')
 // const {refPay} = require('./payment/refPay')
 
-const createRequestId = () => uuid()
+const createRequestId = () => uuid();
 
 Clientdb.connect();
 
 const server = Fastify({
-        ignoreTrailingSlash:true,
-        genReqId: createRequestId,
-        logger:{
-            level: 'info'
-        }
-    })
-    
-    server.register(jwt,{
-        secret: "supersecret"
-    })
+  ignoreTrailingSlash: true,
+  genReqId: createRequestId,
+  logger: {
+    level: "info"
+  }
+});
 
-    server.register(require('fastify-cors'),{
-        origin: '*'
-    })
+server.register(jwt, {
+  secret: "supersecret"
+});
 
-    //API buat Login POST
-    server.post('/auth/login/', validatePostLogin, postLogin)
+server.register(require("fastify-cors"), {
+  origin: "*"
+});
 
-    //API buat Signup POST
-    server.post('/auth/signup/', validatePostSignup, postSignup)
+//API buat Login POST
+server.post("/auth/login/", validatePostLogin, postLogin);
 
-    //API buat invoice Pembayaran POST
-    server.post('/invoices/',validateInvoice, postInvoice)
-    server.post('/invoices/teaminvoice/', getTeamInvoice)
+//API buat Signup POST
+server.post("/auth/signup/", validatePostSignup, postSignup);
 
-    server.post('/checkpayment/', userController)
+//API buat invoice Pembayaran POST
+server.post("/invoices/", validateInvoice, postInvoice);
+server.post("/invoices/teaminvoice/", getTeamInvoice);
 
-    //API buat Pembayaran sukses GET PARAMs
-    server.get('/invoice/success/:emailUser', getSuccess)
-    server.get('/invoice/teamsuccess/:email1/:email2/:email3/:email4/:email5', getTeamSuccess)
+server.post("/checkpayment/", userController);
 
-    //Jalanin Cron Job
-    server.get('/valid',validLicense)
+//API buat Pembayaran sukses GET PARAMs
+server.get("/invoice/success/:emailUser", getSuccess);
+server.get(
+  "/invoice/teamsuccess/:email1/:email2/:email3/:email4/:email5",
+  getTeamSuccess
+);
 
-    //tes fun api
-    //server.get('/tes/:tes', refPay)
-    
-    server.listen(3001, (err) => {
-        if(err){
-            server.log.error(err)
-            console.log(err)
-            process.exit(1)
-        }
-        server.log.info('Server Started')
-    })
+//Jalanin Cron Job
+server.get("/valid", validLicense);
+
+//tes fun api
+//server.get('/tes/:tes', refPay)
+
+server.listen(10000, err => {
+  if (err) {
+    server.log.error(err);
+    console.log(err);
+    process.exit(1);
+  }
+  server.log.info("Server Started");
+});
