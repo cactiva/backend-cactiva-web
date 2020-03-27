8cfcfd2880106bf9769d585ca0f05149345448c5
Erlangga <erlangga@cactiva.app>
2020-03-27 18:03:14 +0700

    fh

--- src/controller/requestresetpw.js
+++ src/controller/requestresetpw.js
@@ -1,31 +1,20 @@
 const { Clientdb } = require('../db/db')
 const jwt = require('jsonwebtoken')
-const fastify = require('fastify')()
+const fastify = require('fastify')
+const nodemailer = require('fastify-nodemailer')
 
-const mailOptions = (email, url) => {
-    const mail = {
-        from: 'Cactiva <erlangga@cactiva.app>',
-        to: email,
-        subject: "Reset Password Request",
-        text: 'Hi, click this link to reset password, the link will expired in 1 hour ' + url,
+fastify.register(nodemailer, {
+    pool: true,
+    host: 'smtp.yandex.com',
+    port: 465,
+    secure: true, // use TLS
+    auth: {
+      user:'erlangga@cactiva.app',
+      pass:'Cactiva123!'
     }
-    return mail
-}
+  })
 
 const sendResetPassword = async (req, res, next) => {
-    
-    fastify.register(require('fastify-nodemailer'), {
-        pool: true,
-        host: 'smtp.yandex.com',
-        port: 465,
-        secure: true, // use TLS
-        auth: {
-          user:'erlangga@cactiva.app',
-          pass: 'Cactiva123!'
-        }
-      })
-    
-    const { nodemailer } = fastify
     const {email} = req.params
 
     try{
@@ -40,11 +29,17 @@ const sendResetPassword = async (req, res, next) => {
         }
         const token = jwt.sign(ids, email, {expiresIn: 3600})
         const url = "https://cactiva.netlify.com/form/resetpassword/?id="+iduser+"&token="+token
-        fastify.nodemailer.sendMail(mailOptions(email, url), (err, info) =>{
+        fastify.nodemailer.sendMail({
+            from: 'Cactiva <erlangga@cactiva.app>',
+            to: email,
+            subject: "Reset Password Request",
+            text: 'Hi, click this link to reset password, the link will expired in 1 hour ' + url,
+        }, (err, info) =>{
             if (err){
                 next(err)
+            }else if(info){
+                res.send("Check your email")
             }
-            res.send("Check your email")
         })
     }catch(err){
         res.send(err)
